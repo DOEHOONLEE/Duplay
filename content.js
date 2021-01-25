ff3f2e8f18d9847fc86d693a5d7aabbc4fa0f094
DOEHOONLEE <genesisparadox@gmail.com>
2021-01-25 21:54:15 +0900

    html content caplitazlied + new logo images added

--- content.js
+++ content.js
@@ -65,14 +65,15 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     // 100 퍼센트 시청한 영상 투명도 조절 함수
     function watchCompleted(shadeCheck){
         const percent_100 = [...document.querySelectorAll('#progress')].filter(percent => percent.style.width === '100%');
-        if( shadeCheck ){
+        if (shadeCheck) {
             percent_100.forEach( video => {
                 video.parentNode.parentNode.parentNode.parentNode.parentNode.style.opacity = '0.4';
-            })
-        }else{
+            });
+        }
+        else {
             percent_100.forEach( video => {
                 video.parentNode.parentNode.parentNode.parentNode.parentNode.style.opacity = '1';
-            })
+            });
         }
     }
 
@@ -81,18 +82,17 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     // 1. 플레이리스트 총 길이 및 시청한 길이 계산
     totalDurationCalc(playlistContainer);
 
-    // 2. 100% 시청한 영상 투명도 조절!
-    // document.querySelector('.switch').addEventListener('click', watchCompleted);
-    // watchCompleted();
-
     // 배열 결과 값 => [총 시간, 시청 시간, 시청 한 시간 (퍼센트)]
     const result = [secToDisplayFormat(totalDuration), secToDisplayFormat(watchedDuration), watchedDuration / totalDuration];
     
     if (request.action == "getDOM") {
         sendResponse({dom: result});
-    }else if (request.action == 'click'){
+    }
+    else if (request.action == 'click') {
+        // 2. 100% 시청한 영상 투명도 조절!
         watchCompleted(request.msg);
-    }else {
+    }
+    else {
         sendResponse({}); // Send nothing..
     }
 });
