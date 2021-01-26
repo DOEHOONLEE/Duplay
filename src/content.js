30f8c8ac725744ae9057f7bf60de431fd4d7fe5c
DOEHOONLEE <genesisparadox@gmail.com>
2021-01-26 16:10:09 +0900

    deprecated api's are now replaced

--- src/content.js
+++ src/content.js
@@ -1,33 +1,30 @@
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-
-                    // [0] DOM selection + 필요 변수들 정의  //
-
-    const playlistContainer = document.querySelectorAll(".ytd-thumbnail-overlay-time-status-renderer");
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+                    // [ 0 ] DOM SELECTION + 필요 변수들 정의 //
     let videoDurations = [];
     let totalDuration = 0;
     let watchedDuration = 0;
 
-                    // [1] 함수들 정의  //
+                    // [ 1 ] 함수 정의 //
 
-    // 플레이리스트 총 시간/길이 계산
+        // 플레이리스트 총 시간/길이 계산
     function totalDurationCalc(playlist) {
-        // 각 비디오의 시간 받아오기
+            // 각 비디오의 시간 받아오기
         for (let i=0; i < playlist.length; i++) {
             if (playlist[i].tagName === "SPAN") {
             videoDurations.push(playlist[i].innerHTML.trim());
             }
         }
 
-        // 각 비디오의 시간 계산 후, 총 길이에 더하기
+            // 각 비디오의 시간 계산 후, 총 길이에 더하기
         videoDurations.forEach(eachDuration => {
             totalDuration += HMStoSecond(eachDuration);
         });
 
-        // 시청한 비디오 계산
+            // 시청한 비디오 계산
         checkWatched();
     }
 
-    // 시청한 비디오 시간 계산
+        // 시청한 비디오 시간 계산
     function checkWatched() {
         const watchedList = [...document.querySelectorAll("#progress")].slice(0,-1);
         watchedList.forEach(eachVideo => {
@@ -47,13 +44,13 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         });
     }
 
-    // HMS 를 초단위로 변환
-        // HMS 파라미터 = HH:MM:SS 형식의 문자열
+        // HMS 를 초단위로 변환
+            // HMS 파라미터 = HH:MM:SS 형식의 문자열
     function HMStoSecond(HMS) {
         return HMS.split(':').reverse().reduce((a,c,idx) => a + Math.pow(60,idx) * c ,0);
     }
 
-    // 초를 HMS 로 변환
+        // 초를 HMS 로 변환
     function secToDisplayFormat(second) {
         let hour = Math.floor(second / 3600);
         let min = Math.floor(second % 3600 / 60);
@@ -62,7 +59,7 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         return `${hour < 10 ? "0" + hour : hour} : ${min < 10 ? "0" + min : min} : ${sec < 10 ? "0" + sec : sec}`;
     }
 
-    // 100 퍼센트 시청한 영상 투명도 조절 함수
+        // 100 퍼센트 시청한 영상 투명도 조절 함수
     function watchCompleted(shadeCheck){
         const percent_100 = [...document.querySelectorAll('#progress')].filter(percent => percent.style.width === '100%');
         if (shadeCheck) {
@@ -77,22 +74,26 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         }
     }
 
-                        // [3] 함수 호출 + 전달 할 값 정의 //
-
+                   // [3] 함수 호출 + 전달 할 값 정의 //
+                   
     // 1. 플레이리스트 총 길이 및 시청한 길이 계산
+    const playlistContainer = document.querySelectorAll(".ytd-thumbnail-overlay-time-status-renderer");
     totalDurationCalc(playlistContainer);
-
+    
     // 배열 결과 값 => [총 시간, 시청 시간, 시청 한 시간 (퍼센트)]
     const result = [secToDisplayFormat(totalDuration), secToDisplayFormat(watchedDuration), watchedDuration / totalDuration];
-    
-    if (request.action == "getDOM") {
-        sendResponse({dom: result});
+
+    if (request.action === "getDuration") {
+        console.log("CONTENT GETDURATION SUCCESS");
+        console.log(playlistContainer)
+        console.log(result)
+        sendResponse({ duration: result })
     }
-    else if (request.action == 'click') {
-        // 2. 100% 시청한 영상 투명도 조절!
-        watchCompleted(request.msg);
+    else if (request.action === "shadeOut") {
+        watchCompleted(request.value);
+        console.log(request.value);
     }
     else {
-        sendResponse({}); // Send nothing..
+        console.log("FAIL");
     }
 });
