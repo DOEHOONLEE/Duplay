534f01c476ba6605ba04b32e3f5e7375a7697067
DOEHOONLEE <genesisparadox@gmail.com>
2021-01-26 13:53:59 +0900

    Revert "Feat/lyukidon"

---
+++ popup.js
@@ -0,0 +1,17 @@
+chrome.tabs.getSelected(null, function(tab) {
+    // Send a request to the content script.
+    chrome.tabs.sendRequest(tab.id, {action: "getDOM"}, function(response) {
+        console.log(response.dom);
+
+        // 배열 결과 값 => [총 시간, 시청 시간, 시청 한 시간 (퍼센트)]
+        const total = response.dom[0];
+        const watched = response.dom[1];
+        const percent = response.dom[2];
+
+        // DOM 에 결과값 넣기
+        document.getElementById("total").innerHTML = total;
+        document.getElementById("watched").innerHTML = watched;
+        document.getElementById("percent").innerHTML = `<strong>${Math.floor(percent*100)}% completed</strong>`;
+        document.getElementById("progress-bar").style.width = `${Math.floor(percent*94)}%`;
+    });
+});
