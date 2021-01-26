60de89836553c25fa64dc3c9649ef103fa3ed749
DOEHOONLEE <genesisparadox@gmail.com>
2021-01-26 13:56:00 +0900

    파일 폴더로 정리

---
+++ src/popup.js
@@ -0,0 +1,21 @@
+chrome.tabs.getSelected(null, function(tab) {
+    // Send a request to the content script.
+    chrome.tabs.sendRequest(tab.id, {action: "getDOM"}, function(response) {
+
+        // 배열 결과 값 => [총 시간, 시청 시간, 시청 한 시간 (퍼센트)]
+        const total = response.dom[0];
+        const watched = response.dom[1];
+        const percent = response.dom[2];
+
+        // DOM 에 결과값 넣기
+        document.getElementById("total").innerHTML = total;
+        document.getElementById("watched").innerHTML = watched;
+        document.getElementById("percent").innerHTML = `<strong>${Math.floor(percent*100)}% completed</strong>`;
+        document.getElementById("progress-bar").style.width = `${Math.floor(percent*94)}%`;
+    });
+
+    const shadeCheck = document.querySelector('#shadeCheck');
+    shadeCheck.addEventListener('click',function(){
+        chrome.tabs.sendRequest(tab.id, {action: "click", msg: shadeCheck.checked})
+    })
+})
