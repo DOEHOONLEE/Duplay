051481c095dcfece713ad4a4c2407dee9a5dde8f
DOEHOONLEE <genesisparadox@gmail.com>
2021-02-03 23:20:33 +0900

    새로고침 버튼 클릭 시 자동으로 데이터 반환

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,21 @@
                 // [ 0 ] DOM SELECTION + 필요 변수들 정의 //
 
 const shadeCheck = document.querySelector('#shadeCheck');
+const refresh = document.querySelector("#refresh");
 
                 // [ 1 ] 함수 정의 //
 
-function responseValidator(arr) {
-    for (let i=0; i < arr.length; i++) {
-        if (arr[i] == null || String(arr[i]).includes("NaN")) {
+function responseValidator(obj) {
+    for (let elem in obj) {
+        if (obj[elem] === null) {
             return false;
+        }
+        else {
+            if (typeof obj[elem] != "number") {
+                if (obj[elem].includes("NaN")) {
+                    return false;
+                }
+            }
         }
     }
     return true;
@@ -25,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const activeTab = tabs[0];
 
         chrome.tabs.sendMessage(activeTab.id, { action: "getDuration" }, function(response) {
-            console.log(response)
+
             if (response != undefined) {
                 // 요청 배열 검사
                 if (responseValidator(response.duration)) {
@@ -56,5 +64,13 @@ document.addEventListener("DOMContentLoaded", function() {
         shadeCheck.addEventListener("click", function() {
             chrome.tabs.sendMessage(activeTab.id, { action: "shadeOut", value: shadeCheck.checked })
         });
+
+        refresh.addEventListener("click", function() {
+            alert("hi");
+            chrome.tabs.sendMessage(activeTab.id, { action: "refresh" });
+            setTimeout(function() {
+                window.close();
+            }, 1500)
+        })
     })
 })
